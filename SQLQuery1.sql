9b2989213dbd7d1c96bc8c8e2b2caa9f5084d290
Kaiky <126119808+KaikyDEV@users.noreply.github.com>
2024-02-25 18:06:17 -0300

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,18 @@
+USE teste;
+
+CREATE TABLE Alunos (
+Id INT NOT NULL PRIMARY KEY, 
+nome VARCHAR(255) NOT NULL,
+serie VARCHAR(255) NOT NULL,
+curso VARCHAR(255) NOT NULL);
+
+INSERT INTO Alunos(Id, nome, serie, curso) VALUES (1, 'Kaiky Santos da Silva', 'Terceiro Ano', 'Analise e Desenvolvimento de Sistemas')
+
+SELECT * FROM Alunos;
+
+USE teste;
+
+SELECT * FROM Alunos;
+
+ALTER TABLE Alunos
+ALTER COLUMN Id INT	IDENTITY(1,1);
